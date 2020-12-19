311ae8adbb8144c088607e24d0148c40b512c1c6
Btissameq <qorchi.btissam@gmail.com>
2020-12-19 19:52:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Modal from "react-modal";
 import './App.css';
 
 function App() {
+  const [isOpen,setIsOpen]=useState(false);
+  Modal.setAppElement('#root');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="open" onClick={()=>setIsOpen(true)}>Click Me</button>
+      <Modal isOpen={isOpen} onRequestClose={()=>setIsOpen(false)} shouldCloseOnOverlayClick={true}>
+        <h3>Duis autem vel eum iriure dolor</h3>
+        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+        <button onClick={()=>setIsOpen(false)}>Close</button>
+      </Modal>
     </div>
   );
 }
